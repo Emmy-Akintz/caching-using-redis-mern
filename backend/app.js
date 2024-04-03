217e380331e62cfa55bc71cfb31c271b30ee7b2c
Ayomide Akintan <emzyakints2005@gmail.com>
2024-04-03 16:11:32 +0100

    working on the backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
+
 //! connect to mongodb
 mongoose.connect(
     process.env.MONGO_CONNECTION_STRING
@@ -8,12 +9,42 @@ mongoose.connect(
     .then(() => console.log(`DB is connected`))
     .catch((e) => console.log(e))
 
-const app = express()
+//! User model
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+}, {
+    timestamps: true,
+})
 
+//! User model
+const User = mongoose.model('User', userSchema)
+
+const app = express()
 
 //! Middlewares
 app.use(express.json())
 
+//! create user route
+app.post('/users/create', async (req, res) => {
+    try {
+        const { name, email } = req.body
+        const user = await User.create({ name, email })
+        res.json(user)
+    } catch (error) {
+        res.send("Server error")
+    }
+})
+
+//! lists
+app.post('/users/lists', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (error) {
+        res.send("Server error")
+    }
+})
 
 //! Start the server
 const PORT = process.env.PORT || 5000
